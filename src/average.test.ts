b5aad788f6035ab4afdf0bf5d198cf45ee797826
utigernils <utigernils@gmail.com>
2025-03-27 23:33:13 +0100

    feat; implemented median

--- src/average.test.ts
+++ src/average.test.ts
@@ -1,4 +1,4 @@
-import { mean } from "./average";
+import { mean, median } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
@@ -24,6 +24,44 @@ test("mean of five numbers", () => {
   expect(actual).toBe(expected);
 });
 
-test('mean of no numbers', () => {
-  expect(() => mean([])).toThrow("Cannot calculate mean of empty array");
+test("mean of no numbers", () => {
+  // Arrange
+  const numbers: number[] = [];
+  const expectedError: string = "Cannot calculate mean of empty array";
+
+  // Act & Assert
+  expect(() => mean(numbers)).toThrow(expectedError);
+});
+
+test("median for odd number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 6, 7, 8];
+  const expected: number = 6;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("median for even number of elements", () => {
+  // Arrange
+  const numbers: number[] = [1, 2, 5, 6, 7, 8];
+  const expected: number = 5.5;
+
+  // Act
+  const actual: number = median(numbers);
+
+  // Assert
+  expect(actual).toBe(expected);
+});
+
+test("median for no elements", () => {
+  // Arrange
+  const numbers: number[] = [];
+  const expectedError: string = "Cannot calculate median of empty array";
+
+  // Act & Assert
+  expect(() => median(numbers)).toThrow(expectedError);
 });
--- src/average.ts
+++ src/average.ts
@@ -2,11 +2,24 @@ export function mean(numbers: number[]): number {
   if (numbers.length === 0) {
     throw new Error("Cannot calculate mean of empty array");
   }
-  
+
   let sum: number = 0;
   for (let number of numbers) {
     sum += number;
   }
   const mean: number = sum / numbers.length;
   return mean;
+}
+
+export function median(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Cannot calculate median of empty array");
+  }
+  
+  const sorted = [...numbers].sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+  
+  return sorted.length % 2 === 0 
+    ? (sorted[middle - 1] + sorted[middle]) / 2 
+    : sorted[middle];
 }
