5824ea944dd97695e7f87166d5c6d7196efcf9d2
utigernils <utigernils@gmail.com>
2025-03-27 23:19:53 +0100

    fix; changed values in the test case

--- src/average.test.ts
+++ src/average.test.ts
@@ -2,8 +2,8 @@ import { mean } from "./average";
 
 test("test mean of two numbers", () => {
   // Arrange
-  const numbers: number[] = [1, 4];
-  const expected: number = 2.5;
+  const numbers: number[] = [4, 5];
+  const expected: number = 4.5;
 
   // Act
   const actual: number = mean(numbers);
